c311061a9bb4847a392e1726a4d7f91fec1baa33
LilyMart7 <lilymart7@gmail.com>
2021-01-06 10:50:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// src/App.tsx
+
+import React from 'react';
+import './App.scss';
+import Hello from './layout/hello/hello';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+
+
+  return (
+ <Router>
+      <div>
+
+        <Route exact path="/hello" component={Hello} />
+        {/* <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/forgotPassword" component={ForgotPassword} />
+        <Route exact path="/register" component={SignUp} /> */}
+      </div>
+    </Router>
+  )
+}
+
+export default App;
